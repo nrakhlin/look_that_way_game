0a2e7d5a1ae1aaa54bb4a5235c903c6f6730f891
npr29 <npr29@cornell.edu>
2024-11-14 18:15:30 -0500

    finally

--- game_dpad.py
+++ game_dpad.py
@@ -88,68 +88,78 @@ def get_user_input():
   print(f"ai point direction: {ai_point}")
   return usr_point, ai_point
 
-while(1):
-  print(f"Score: {score}")
-  print(f"Level: {level}")
-  usr_point = ""
-  ai_point = ""
-  #currently in state x
-  if state == States.INIT:
-    print("Welcome to Game")
-    pass
 
-  elif state == States.USR_POINTER:
-    print("User_pointer")
-    while(usr_point == ""):
-       pass
+ready = False
+
+try:
+  while(1):
+    print(f"Score: {score}")
+    print(f"Level: {level}")
+    usr_point = ""
+    ai_point = ""
+    #currently in state x
+    if state == States.INIT:
+      print("Welcome to Game")
+      pass
+
+    elif state == States.USR_POINTER:
+      print("User_pointer")
+      ready = True
+      while(usr_point == ""):
+        pass
+      time.sleep(1)
+      ai_point = Directions.select_random_direction()
+      print(f"ai point direction: {ai_point}")
+      # usr_point, ai_point = get_user_input()
+    
+    elif state == States.USR_LOOK:
+      print("user_looker")
+      while(usr_point == ""):
+        pass
+      time.sleep(1)
+      ai_point = Directions.select_random_direction()
+      print(f"ai point direction: {ai_point}")
+      # usr_point, ai_point = get_user_input()
+
+    elif state == States.GAMEOVER:
+      print("Sorry, you loose")
+      print(f"Final Score: {score}")
+      print(f"Final level: {level}")
+      break
+
     time.sleep(1)
-    ai_point = Directions.select_random_direction()
-    print(f"ai point direction: {ai_point}")
-    # usr_point, ai_point = get_user_input()
-  
-  elif state == States.USR_LOOK:
-    print("user_looker")
-    while(usr_point == ""):
-       pass
-    time.sleep(1)
-    ai_point = Directions.select_random_direction()
-    print(f"ai point direction: {ai_point}")
-    # usr_point, ai_point = get_user_input()
 
-  elif state == States.GAMEOVER:
-    print("Sorry, you loose")
-    print(f"Final Score: {score}")
-    print(f"Final level: {level}")
-    break
-
-  time.sleep(1)
-
-  #handle state transition from 
-  if state == States.INIT:
-    next_state = States.USR_POINTER
-  if state == States.USR_POINTER:
-    if usr_point == ai_point:
-      score += score_multiplier * points_per_score
-      next_state = state
-      print("Great job, you gottem! Try to catch this individual once more!!")
-    else:
-      next_state = States.USR_LOOK
-      print("Nice try but you missed, Mr.")
-      print("Switch sides!")
-  
-  if state == States.USR_LOOK:
-
-    if usr_point == ai_point:
-      next_state = States.GAMEOVER
-      print("The AI got you why would you let it do that!!")
-    else:
-      countdown = countdown - countdown * 0.2
-      score_multiplier = score * 1.1
-      level += 1
+    #handle state transition from 
+    if state == States.INIT:
       next_state = States.USR_POINTER
-      print("Nice job, you evaded the AI!!")
+    if state == States.USR_POINTER:
+      if usr_point == ai_point:
+        score += score_multiplier * points_per_score
+        next_state = state
+        print("Great job, you gottem! Try to catch this individual once more!!")
+      else:
+        next_state = States.USR_LOOK
+        print("Nice try but you missed, Mr.")
+        print("Switch sides!")
+    
+    if state == States.USR_LOOK:
 
-  time.sleep(1)
-  print("\n")
-  
-  state = next_state
+      if usr_point == ai_point:
+        next_state = States.GAMEOVER
+        print("The AI got you why would you let it do that!!")
+      else:
+        countdown = countdown - countdown * 0.2
+        score_multiplier = score_multiplier * 1.1
+        level += 1
+        next_state = States.USR_POINTER
+        print("Nice job, you evaded the AI!!")
+
+    time.sleep(1)
+    print("\n")
+    
+    state = next_state
+
+except KeyboardInterrupt:
+   pass
+finally:
+   GPIO.cleanup
